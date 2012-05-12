c0b0e80d9e46d33e3db9d69ee0bc77d3fd74d0bf
unknown <Administrator@asus-PC.(none)>
2012-05-12 15:47:42 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Orm1::Application.config.secret_token = '6ca75648efe83f2ca8a39f7160468f7646fd676b9f5ec0466efe1e82c6a54346b052c21ac4511e129be3f13cd3992af0337f7fb805aa5e3010bed9d8ce971f86'

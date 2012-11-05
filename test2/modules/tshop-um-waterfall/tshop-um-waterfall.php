a33effdc91964509b095247758fabbfd45802d9d
wisteriaflash <wisteriaflash@163.com>
2012-11-05 17:36:36 +0800

    瀑布流样式微调

--- test2/modules/tshop-um-waterfall/tshop-um-waterfall.php
+++ test2/modules/tshop-um-waterfall/tshop-um-waterfall.php
@@ -16,14 +16,6 @@
 /**
  * 开始设计PHP页面
  */
-  //test data
-  // $arr = array(
-  // "http://img04.taobaocdn.com/bao/uploaded/i4/T1HC_oXcJkXXX0Bi70_035639.jpg_310x310.jpg",
-  // "http://img03.taobaocdn.com/bao/uploaded/i3/T1QYnoXedsXXcokjc1_041606.jpg_220x220.jpg"
-  // );
-
-
-  //
   extract($_MODULE, EXTR_PREFIX_ALL | EXTR_OVERWRITE, 'tbm');
   //main
   $pageLinks =$shopManager->getShopPageLinks();
@@ -61,84 +53,25 @@
         //items
         foreach ($items as $k => $v) {
           $item = $v;
+          $url = $uriManager->detailURI($item);
           $defPic = $itemsPic[$k];
           $pic = $defPic ? $defPic : $item->getPicUrl($itemWidth);
           $price = $item->price;
           $title = $item->title;
           $sale =$item->soldCount;
-          $url = $uriManager->detailURI($item);
+          $collect = $item->collectedCount;
           echo "<li>".
               "<div class='pic'><a href='{$url}' target='_blank'><img src='{$pic}' /></a></div>".
-              "<div class='info'>".
+              "<div class='detail'>".
+                "<div class='price'><i></i>￥".$price.".00</div>".
+                "<div class='salenum'>".
+                  "<span class='count'><i></i>宝贝已售出：<em>".$sale."</em>笔</span><span class='colect'><i></i>人气收藏：<em>".$collect."</em>人</span>".
+                "</div>".
               "</div>".
             "</li>";
         }
         echo "</ul>";
       }
     ?>
-</div>
-
-<!-- <div class="pubu">
-
-
-<?for ($i=0;$i<4;$i++){
-  $ids = explode(',',$_MODULE['idem'.$i]);
-  $idem =  $itemManager->queryByIds($ids,''); 
-
-  if(count($idem)<=0) {
-  $idem = $itemManager->queryByKeyword ("","hotsell",3); 
-  }
-  $zdtu = explode('|',$_MODULE['zdtu'.$i]); 
-?> 
-
-
-  <ul class="<?if($i == 3) echo 'end'  ?> " style="display:none;">
-  
-    <?for ($c=0;$c<count($idem);$c++){ ?>
-  
-    <li>
-      <h4>
-          <a href="<? if($idem[$c]) echo $uriManager->detailURI($idem[$c]) else echo '#'  ?> " target="_blank" style="display:block;">
-            <img style = "float:left;display:block;width:210px;" src = "<?if($zdtu[$c]) echo $zdtu[$c] else echo $arr[0]  ?> "/>
-                </a>      
-      
-      </h4>
-      
-
-      
-      <h3>
-        <span>
-        
-            <a href="http://favorite.taobao.com/popup/add_collection.htm?id=<?php echo $idem[$c]->id; ?>&itemtype=1&scjjc=1">
-                <img src="http://img04.taobaocdn.com/L1/142/1097795/modules/tshop-um-pubu950/assets/images/pubu950_sc.jpg">
-          </a>
-          
-          <a href="<? if($idem[$c]) echo $uriManager->detailURI($idem[$c]) else echo '#'  ?>">
-          
-              <img src="http://img04.taobaocdn.com/L1/142/1097795/modules/tshop-um-pubu950/assets/images/pubu950_gm.jpg">
-          </a>
-          
-          
-        </span>
-        <strong>￥:<?if($idem[$c]) echo $idem[$c]->price else echo '200'  ?> </strong>
-        已售<? if($idem[$c]) echo $idem[$c]->soldCount else echo '300' ?> 件
-        
-        
-      </h3>
-      
-      
-      
-    </li>
-    
-    
-    <? }?> 
-  
-  </ul>
-
-  
-<?}?> 
-   -->
-  
-  
-  
+  </div>
 </div>

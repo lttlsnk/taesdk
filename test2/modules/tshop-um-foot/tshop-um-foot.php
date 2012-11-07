5bfa05e9fedfef3e31e0864c50847b959e251ceb
wisteriaflash <wisteriaflash@163.com>
2012-11-07 08:46:33 +0800

    底部模块调整

--- test2/modules/tshop-um-foot/tshop-um-foot.php
+++ test2/modules/tshop-um-foot/tshop-um-foot.php
@@ -16,25 +16,75 @@
 /**
  * 开始设计PHP页面
  */
+  extract($_MODULE, EXTR_PREFIX_ALL | EXTR_OVERWRITE, 'tbm');
+  //self value
+  $shareConfig = getShareConfig("shop",$shopId,$shopTitle);
+  //service
+  $defaulNickArray = array($userNick, $userNick, $userNick);
+  $defaulIdArray = array(1,2,3);
+  //
+  $serviceName1 = $tbm_servicenick1 ? explode("|",$tbm_servicenick1) : $defaulNickArray;
+  $serviceID1 = $tbm_serviceid1 ? explode("|", $tbm_serviceid1) : $defaulIdArray;
+  $serviceName2 = $tbm_servicenick2 ? explode("|",$tbm_servicenick2) : $defaulNickArray;
+  $serviceID2 = $tbm_serviceid2 ? explode("|", $tbm_serviceid2) : $defaulIdArray;
 ?>
-<!-- <div class="skin-box">
-  <div class="skin-box-hd">
-    <h3><span>士大夫士大夫</span></h3>
-    <p class="title">这是测试标题</p>
+  <div class="ft-nav">
+    <div class="ft-nav-con">
+      <ul>
+        <li><a href="<?=$shopUrl ?>">返回首页</a> | </li>
+        <li><a href="<?=$allProduct ?>">所有宝贝</a> | </li>
+        <li><a href="<?=$shopRate ?>">信用评价</a> | </li>
+        <li><a href="<?=$shopIntr ?>">关于我们</a> | </li>
+        <li><a href="<?=$shopFavorite ?>">收藏本店</a> | </li>
+        <li><a href="#top">返回顶部</a></li>
+      </ul>
+    </div>
   </div>
-</div> -->
-<div class="ft-nav">
-  <div class="ft-nav-con">
-    <ul>
-      <li><a href="<?php echo $shopUrl ?>">返回首页</a> | </li>
-      <li><a href="<?php echo $uriManager->searchURI(); ?>">所有宝贝</a> | </li>
-      <li><a href="<?php echo $uriManager->rateURI(); ?>">信用评价</a> | </li>
-      <li><a href="<?php echo $uriManager->shopIntrURI(); ?>">关于我们</a> | </li>
-      <li><a href="<?php echo $uriManager->favoriteLink(); ?>">收藏本店</a> | </li>
-      <li><a href="#top">返回顶部</a></li>
-    </ul>
+  <div class="ft-info">
+    <div class="ft-info-detail">
+      <ul>
+        <?php
+          for($i=1; $i<=4; $i++){
+            $head = ${'tbm_abouthead'.$i};
+            $content = ${'tbm_aboutcontent'.$i};
+            echo "<li>".
+                "<span class='title'>{$head}</span>".
+                "<span class='content'>{$content}</span>".
+              "</li>";
+          }
+        ?>
+      </ul>
+    </div>
+    <div class="ft-info-share">
+        <p class="title">分享店铺到：</p>
+        <div class="sns-widget" data-sharebtn='<?=$shareConfig?>'></div>
+    </div>
+    <div class="ft-info-wangwang">
+      <ul>
+        <li>
+          <span class="title"><?=$tbm_servicehead1?>：</span>
+          <?php
+            foreach ($serviceID1 as $k => $v) {
+              $itemID = $v;
+              $itemName = $serviceName1[$k];
+              $wangwang = $uriManager->supportTag($itemID,$itemName,$tbm_wangwangskin,false);
+              echo "<span><em title='请点击右侧头像'>{$itemName}</em>{$wangwang}</span>";
+            }
+          ?>
+        </li>
+        <li>
+          <span class="title"><?=$tbm_servicehead2?>：</span>
+          <?php
+            foreach ($serviceID2 as $k => $v) {
+              $itemID = $v;
+              $itemName = $serviceName1[$k];
+              $wangwang = $uriManager->supportTag($itemID,$itemName,$tbm_wangwangskin,false);
+              echo "<span><em title='请点击右侧头像'>{$itemName}</em>{$wangwang}</span>";
+            }
+          ?>
+        </li>
+        <li class="worktime"><?=$tbm_worktime ?></li>
+      </ul>
+    </div>
   </div>
-</div>
-
-
 </div>
